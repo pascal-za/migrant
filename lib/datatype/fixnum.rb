27aeb7b68bb4f19b84e8d2123650fc4966c9a6fb
Ryan & Pascal <ryan+pascal@platform45.com>
2011-03-31 11:23:14 +0200

    Automatically generate larger integer storage when large examples are provided

--- lib/datatype/fixnum.rb
+++ lib/datatype/fixnum.rb
@@ -1,7 +1,9 @@
 module DataType
   class Fixnum < Base
     def column
-      {:type => :integer}
+      {:type => :integer}.tap do |options|
+        options.merge!(:limit => @value.size) if @value > 2147483647 # 32-bit limit. Not checking size here because a 64-bit OS always has at least 8 byte size
+      end
     end
     
     def self.default_mock
