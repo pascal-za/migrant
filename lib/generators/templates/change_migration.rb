ffa140fd2d1a68fbf92b4283c8dc31a73d68089e
Pascal Houliston <101pascal@gmail.com>
2011-04-09 19:58:19 +0000

    Refactor all output into Erubis

--- lib/generators/templates/change_migration.rb
+++
@@ -1,9 +0,0 @@
-class <%= @activity.camelize.gsub(/\s/, '') %> < ActiveRecord::Migration
-  def self.up
-    <%= @up_steps.join("\n    ") %>
-  end
-  
-  def self.down
-    <%= @down_steps.join("\n    ") %>
-  end
-end
--- lib/migrant/migration_generator.rb
+++ lib/migrant/migration_generator.rb
@@ -1,3 +1,5 @@
+require 'erubis'
+
 module Migrant
   class MigrationGenerator
     TABS = '  ' # Tabs to spaces * 2
@@ -22,6 +24,7 @@ module Migrant
         next if model.schema.nil? || !model.schema.requires_migration? # Skips inherited schemas (such as models with STI)
         model.reset_column_information # db:migrate doesn't do this
         model_schema = model.schema.column_migrations
+        @table_name = model.table_name
 
         if model.table_exists?
           # Structure ActiveRecord::Base's column information so we can compare it directly to the schema
@@ -35,55 +38,33 @@ module Migrant
               end
             end.reject { |change| change[1].nil? }
             next if changes.blank?
-            changed_fields, added_fields = [], []
-
-            @up_steps = changes.collect do |field, options|
-              type = options.delete(:type)
-              arguments = (options.blank?)? "" : ", #{options.inspect[1..-2]}"
-
+            @changed_columns, @added_columns = [], []
+               
+            # Fields to add/remove         
+            changes.each do |field, options|
               if db_schema[field]
-                 changed_fields << field
-                "change_column :#{model.table_name}, :#{field}, :#{type}#{arguments}"
+                @changed_columns << [field, options, db_schema[field]]
               else
-                added_fields << field
-                "add_column :#{model.table_name}, :#{field}, :#{type}#{arguments}"
-              end
-            end
-
-            @activity = 'changed_'+model.table_name+[(added_fields.blank?)? nil : '_added_'+added_fields.join('_'), (changed_fields.blank?)? nil : '_modified_'+changed_fields.join('_')].compact.join('_and_')
-            @activity = @activity.split('_')[0..2].join('_') if @activity.length >= 240 # Most filesystems will raise Errno::ENAMETOOLONG otherwise
-            
-            @down_steps = changes.collect do |field, options|
-              if db_schema[field]
-                type = db_schema[field].delete(:type)
-                arguments = (db_schema[field].blank?)? "" : ", #{db_schema[field].inspect[1..-2]}"
-                "change_column :#{model.table_name}, :#{field}, :#{type}#{arguments}"
-              else
-                "remove_column :#{model.table_name}, :#{field}"
+                @added_columns << [field, options]
               end
             end
 
             # For adapters that can report indexes, add as necessary
             if ActiveRecord::Base.connection.respond_to?(:indexes)
               current_indexes = ActiveRecord::Base.connection.indexes(model.table_name).collect { |index| (index.columns.length == 1)? index.columns.first.to_sym : index.columns.collect(&:to_sym) }
-              @up_steps += model.schema.indexes.uniq.collect do |index|
-                unless current_indexes.include?(index)
-                  "add_index :#{model.table_name}, #{index.inspect}"
-                end
-              end.compact
+              @indexes = model.schema.indexes.uniq.reject { |index| current_indexes.include?(index) }
             end
+
+            # Example: changed_table_added_something_and_modified_something
+            @activity = 'changed_'+model.table_name+[['added', @added_columns], ['modified', @changed_columns]].reject { |v| v[1].empty? }.collect { |v| "_#{v[0]}_"+v[1].collect(&:first).join('_') }.join('_and_')
+            @activity = @activity.split('_')[0..2].join('_') if @activity.length >= 240 # Most filesystems will raise Errno::ENAMETOOLONG otherwise
+            
             render('change_migration')
         else
           @activity = "create_#{model.table_name}"
-          @columns = model_schema.collect do |field, options|
-            type = options.delete(:type)
-            options.delete(:was) # Aliases not relevant when creating a new table
-            arguments = (options.blank?)? "" : ", #{options.inspect[1..-2]}"
-            "t.#{type} :#{field}#{arguments}"
-          end
-
-          @indexes = model.schema.indexes.collect { |fields| "add_index :#{model.table_name}, #{fields.inspect}"}
-          @table_name = model.table_name
+          @columns = model_schema
+          @indexes = model.schema.indexes
+          
           render("create_migration")
         end
        
@@ -117,7 +98,7 @@ module Migrant
     end
 
     def render(template_name)
-      @output = ERB.new(File.read(File.join(File.dirname(__FILE__), "../generators/templates/#{template_name}.rb"))).result(binding)
+      @output = Erubis::Eruby.new(File.read(File.join(File.dirname(__FILE__), "../generators/templates/#{template_name}.erb"))).result(binding)
     end
   end
 end
